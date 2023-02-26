13ed0c0d5bceac229c05c11568789b70a58a7113
zooofzane <yu_an12@163.com>
2023-02-25 19:36:57 -0500

    setup the sculpture

--- main.js
+++ main.js
@@ -1,28 +1,121 @@
-console.log("1")
-
 import * as THREE from 'three'
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 const canvas = document.querySelector('canvas.webgl')
 
-console.log(OBJLoader)
-
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xffffff); // Set the background color to white
 
-const material = new THREE.MeshBasicMaterial()
-const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
-for (let i = 0; i < 100; i++) {
-    const donut = new THREE.Mesh(donutGeometry, material)
-    donut.position.x = (Math.random() - 0.5) * 10
-    donut.position.y = (Math.random() - 0.5) * 10
-    donut.position.z = (Math.random() - 0.5) * 10
-    donut.rotation.x = Math.random() * Math.PI
-    donut.rotation.y = Math.random() * Math.PI
-    const scale = Math.random()
-    donut.scale.set(scale, scale, scale)
+const loader = new GLTFLoader();
 
-    scene.add(donut)
-}
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( './draco/' );
+loader.setDRACOLoader( dracoLoader );
+const col = [0xD3CF2F,0xD3CD2E,0xD1B72C,0xC85F27,0xC34A27,0xC98C33,0x6D532B,0x1F3D74,0x0E2C66,0x035B3E]
+const material=[]
+for(let i =0;i<10;i++){
+    material[i] = new THREE.MeshStandardMaterial({
+    color: col[i], // Yellow color
+    roughness: 0.5,
+});}
+
+
+// let childg =[]
+const logogroup = new THREE.Group();
+
+loader.load(
+	'./assets/1.glb',
+    function (gltf) {
+        gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+                for (let i = 0; i < 10; i++) {
+                    let scaleindex = 1 +i*0.04
+                    let positionindex = 1 - i*0.2
+                    child.material = material[i];
+                    child.castShadow =true
+                    child.receiveShadow =true
+                    const clone = child.clone();
+                    clone.scale.set(scaleindex,scaleindex,scaleindex)
+                    clone.position.set(1,1,positionindex)
+                    logogroup.add(clone)
+                    scene.add(clone);
+                }
+                for (let i = 0; i < 10; i++) {
+                    let scaleindex = 1 +i*0.04
+                    let positionindex = 1 + i*.13
+                    let positionindexZ = 1 - i*.13
+                    child.material = material[i];
+                    child.castShadow =true
+                    child.receiveShadow =true
+                    const clone = child.clone();
+                    clone.scale.set(scaleindex,scaleindex,scaleindex)
+                    clone.position.set(1,positionindex+.5,positionindexZ)
+                    clone.rotation.set(Math.PI / 4,0,0);
+                    logogroup.add(clone)
+                    scene.add(clone);
+                }
+                for (let i = 0; i < 10; i++) {
+                    let scaleindex = 1 +i*0.04
+                    let positionindex = 1 - i*.13
+                    let positionindexZ = 1 - i*.13
+                    child.material = material[i];
+                    child.castShadow =true
+                    child.receiveShadow =true
+                    const clone = child.clone();
+                    clone.scale.set(scaleindex,scaleindex,scaleindex)
+                    clone.position.set(1,positionindex-.5,positionindexZ)
+                    clone.rotation.set(-Math.PI / 4,0,0);
+                    logogroup.add(clone)
+                    scene.add(clone);
+                }
+            }
+        });
+    }
+	);
+
+// const logogroup2 = logogroup.clone();
+// logogroup2.rotation.set(Math.PI / 4, 0, Math.PI / 2);
+// console.log(logogroup)
+// scene.add(logogroup2)
+
+let sG = new THREE.SphereBufferGeometry(.2)
+let s = new THREE.Mesh(sG,material)
+s.castShadow = true
+// scene.add(s)
+
+let s2G = new THREE.SphereBufferGeometry(-0.4)
+let s2 = new THREE.Mesh(s2G,material)
+s2.position.set(0,0),1
+s2.receiveShadow = true
+// scene.add(s2)
+
+
+
+const light1 = new THREE.DirectionalLight(0xffffff, .7); // Color, Intensity
+const light2 = new THREE.DirectionalLight(0xffffff, .4); // Color, Intensity
+const light3 = new THREE.DirectionalLight(0xffffff, .4); // Color, Intensity
+light1.position.set(0, 0.1, 2); // x, y, z
+light2.position.set(0, 0.5, 1); // x, y, z
+light3.position.set(0, -0.5, 1); // x, y, z
+light1.castShadow = true; // Enable shadow casting
+// light2.castShadow = true; // Enable shadow casting
+// light3.castShadow = true; // Enable shadow casting
+light1.shadow.mapSize.width = 1024;
+light1.shadow.mapSize.height = 1024;
+light2.shadow.mapSize.width = 1024;
+light2.shadow.mapSize.height = 1024;
+light3.shadow.mapSize.width = 1024;
+light3.shadow.mapSize.height = 1024;
+scene.add(light1);
+// scene.add(light2);
+// scene.add(light3);
+const lighth = new THREE.HemisphereLight( 0xffffff, 0.5 ); 
+scene.add(lighth);
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -31,13 +124,20 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 camera.position.x = 1
 camera.position.y = 1
 camera.position.z = 2
+
 scene.add(camera)
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.physicallyCorrectLights = true;
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 const clock = new THREE.Clock()
 
@@ -45,7 +145,7 @@ const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
-    // controls.update()
+    controls.update()
     // camera.position.x += (mouseX - camera.position.x) * .05;
     // camera.position.y += (-mouseY - camera.position.y) * .05;
     // console.log("tick")
@@ -61,3 +161,11 @@ const tick = () => {
 }
 
 tick()
+
+
+
+const gridHelper = new THREE.GridHelper( 10, 10 );
+// scene.add( gridHelper );
+const axesHelper = new THREE.AxesHelper( 5 );
+axesHelper.setColors (  0xff0000, 0xffff00, 0x0000ff)
+// scene.add( axesHelper );
