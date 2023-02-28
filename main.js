16c798a3f16e116d3662cdacf29cb8954e5a6c34
zooofzane <yu_an12@163.com>
2023-02-28 16:24:56 -0500

    deploy test

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+// import glbfile from './assets/1.glb'
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -26,6 +27,9 @@ const logogroup = new THREE.Group();
 
 loader.load(
 	'./assets/1.glb',
+    // 'https://drive.google.com/file/d/1_68OC-4vO_Pk2Btf8lStnTDWYSStfvDC/view?usp=share_link',
+    // 'https://www.dropbox.com/s/5sj7ms4k81qmj23/1.glb?dl=0',
+    // glbfile,
     function (gltf) {
         gltf.scene.traverse(function (child) {
             if (child.isMesh) {
