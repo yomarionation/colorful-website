7bdea37f71149d55aa77a9c9f1bd4068e094a261
zooofzane <yu_an12@163.com>
2023-03-01 12:35:38 -0500

    scroll animation direction 1

--- main.js
+++ main.js
@@ -2,33 +2,84 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import Stats from 'three/addons/libs/stats.module.js';
 import model from './public/1.glb?url'
-import  "./public/draco/draco_decoder.wasm?raw"
-import  "./public/draco/draco_wasm_wrapper.js?raw"
+// import draco from 'draco3d';
+// import {setLoaderOptions} from '@loaders.gl/core';
+// setLoaderOptions({
+//   modules: {
+//     'draco_wasm_wrapper.js': require('@loaders.gl/draco/libs/draco_wasm_wrapper.js'),
+//     'draco_decoder.wasm': require('@loaders.gl/draco/libs/draco_decoder.wasm') // NOTE: importing `wasm` requires bundler config
+//   }
+// });
+// import * as dracoloader from 'draco3d/draco_decoder.wasm'
+// import  "./public/draco/draco_decoder.wasm?raw"
+// import  "./public/draco/draco_wasm_wrapper.js?raw"
 // import * as f from "./public/draco/draco_decoder.wasm?raw"
 // import * as a from "./public/draco/draco_wasm_wrapper.js?raw"
+
+/* -------------------------------------------------------------------------- */
+/*                                 Basic Setup                                */
+/* -------------------------------------------------------------------------- */
+let p = [25,50,60,85]
+let mouseX = 0, mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+const stats = Stats()
+document.body.appendChild(stats.dom)
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 const canvas = document.querySelector('canvas.webgl')
 
 const scene = new THREE.Scene()
-// scene.background = new THREE.Color(0xffffff); // Set the background color to white
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+camera.position.set(3.1,-0.1,3.3)
+let a=[3.1,-0.1,3.3]
+let b=[-0.3,1.5,3.2]
+let c=[-2.3,0.9,1.8]
+let cpx = document.getElementById('x')
+let cpy = document.getElementById('y')
+let cpz = document.getElementById('z')
+let camerapos = camera.position
+scene.add(camera)
+
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 const loader = new GLTFLoader();
-
-// Optional: Provide a DRACOLoader instance to decode compressed mesh data
 const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath( './public/draco/' );
+dracoLoader.setDecoderPath( './draco/' );
 loader.setDRACOLoader( dracoLoader );
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                              type refine here                              */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------------ type material ----------------------------- */
 const col = [0xD3CF2F,0xD3CD2E,0xD1B72C,0xC85F27,0xC34A27,0xC98C33,0x6D532B,0x1F3D74,0x0E2C66,0x035B3E]
 const material=[]
 for(let i =0;i<10;i++){
     material[i] = new THREE.MeshStandardMaterial({
-    color: col[i], // Yellow color
+    color: col[i],
     roughness: 0.5,
 });}
 
-const logogroup = new THREE.Group();
-
+/* ------------------------------ type position ----------------------------- */
 loader.load(
 	model,
     function (gltf) {
@@ -43,7 +94,6 @@ loader.load(
                     const clone = child.clone();
                     clone.scale.set(scaleindex,scaleindex,scaleindex)
                     clone.position.set(1,1,positionindex)
-                    logogroup.add(clone)
                     scene.add(clone);
                 }
                 for (let i = 0; i < 10; i++) {
@@ -57,7 +107,6 @@ loader.load(
                     clone.scale.set(scaleindex,scaleindex,scaleindex)
                     clone.position.set(1,positionindex+.5,positionindexZ)
                     clone.rotation.set(Math.PI / 4,0,0);
-                    logogroup.add(clone)
                     scene.add(clone);
                 }
                 for (let i = 0; i < 10; i++) {
@@ -71,7 +120,6 @@ loader.load(
                     clone.scale.set(scaleindex,scaleindex,scaleindex)
                     clone.position.set(1,positionindex-.5,positionindexZ)
                     clone.rotation.set(-Math.PI / 4,0,0);
-                    logogroup.add(clone)
                     scene.add(clone);
                 }
             }
@@ -79,19 +127,10 @@ loader.load(
     }
 	);
 
-let sG = new THREE.SphereBufferGeometry(.2)
-let s = new THREE.Mesh(sG,material)
-s.castShadow = true
-// scene.add(s)
 
-let s2G = new THREE.SphereBufferGeometry(-0.4)
-let s2 = new THREE.Mesh(s2G,material)
-s2.position.set(0,0),1
-s2.receiveShadow = true
-// scene.add(s2)
-
-
-
+/* -------------------------------------------------------------------------- */
+/*                                 light setup                                */
+/* -------------------------------------------------------------------------- */
 const light1 = new THREE.DirectionalLight(0xffffff, .7); // Color, Intensity
 const light2 = new THREE.DirectionalLight(0xffffff, .4); // Color, Intensity
 const light3 = new THREE.DirectionalLight(0xffffff, .4); // Color, Intensity
@@ -113,33 +152,10 @@ scene.add(light1);
 const lighth = new THREE.HemisphereLight( 0xffffff, 0.5 ); 
 scene.add(lighth);
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-camera.position.x = 3.1
-camera.position.y = -.1
-camera.position.z = 3.3
-// 3.1,-0.1,3.3
-// -0.3,1.5,3.2
-// -2.3,0.9,1.8
-scene.add(camera)
-
+/* -------------------------------------------------------------------------- */
+/*                           render & control setup                           */
+/* -------------------------------------------------------------------------- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true,
@@ -149,62 +165,19 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-// renderer.setClearColor(0x000000, 10);
-// renderer.physicallyCorrectLights = true;
 
 const controls = new OrbitControls( camera, renderer.domElement );
+controls.enabled = false
+controls.enableZoom = false
 
 const clock = new THREE.Clock()
 
-let cpx = document.getElementById('x')
-let cpy = document.getElementById('y')
-let cpz = document.getElementById('z')
-let camerapos = camera.position
 
-let scroll = document.getElementById('scroll')
-const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-
-
-    // console.log(camerapos)
-
-    // Update controls
-    // controls.update()
-    // camera.position.x += (mouseX - camera.position.x) * .05;
-    // camera.position.y += (-mouseY - camera.position.y) * .05;
-    // console.log("tick")
-    // camera.lookAt(scene.position);
-    // cp.innerHTML=camerapos.x.toString()
-   
-    // console.log(camerapos.x.toString())
-    
-    // Render
-    renderer.render(scene, camera)
-
-
-    cpx.innerHTML="x: " + camerapos.x.toString()
-    cpy.innerHTML="y: " + camerapos.y.toString()
-    cpz.innerHTML="z: " + camerapos.z.toString()
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-
-// const c = document.createElement("span");
-// c.setAttribute('id','camerap');
-
-
-
-
-const gridHelper = new THREE.GridHelper( 10, 10 );
-// scene.add( gridHelper );
-const axesHelper = new THREE.AxesHelper( 5 );
-axesHelper.setColors (  0xff0000, 0xffff00, 0x0000ff)
-// scene.add( axesHelper );
-
-
-
+/* -------------------------------------------------------------------------- */
+/*                               scroll function                              */
+/* -------------------------------------------------------------------------- */
 let scrollPercent = 0;
+let scroll = document.getElementById('scroll')
 document.body.onscroll = () => {
     scrollPercent =
         ((document.documentElement.scrollTop || document.body.scrollTop) /
@@ -216,12 +189,245 @@ document.body.onscroll = () => {
     scroll.innerHTML="scroll progress: " + scrollPercent.toFixed(2)
 }
 
+// let time = 0
+/* ------------------------------- scroll down ------------------------------ */
+const animationScripts = [];
+animationScripts.push({
+    start: 0,
+    end: p[0],
+    func: function() {
+      camera.lookAt(0,0,0)
+      camera.position.set(a[0],a[1],a[2])
+    }
+  });
+  animationScripts.push({
+    start: 0,
+    end: 110,
+    func: function() {
+        camera.lookAt(0,0,0)
+    camera.position.x += ( mouseX/10000 - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY/10000 - camera.position.y ) * .05;
+    }
+  });
+
+animationScripts.push({
+  start: p[0],
+  end: p[1],
+  func: function() {
+    camera.lookAt(0,0,0)
+    camera.position.x = lerp(camera.position.x, b[0], scalePercent(p[0], p[1]));
+        camera.position.y = lerp(camera.position.y, b[1], scalePercent(p[0], p[1]));
+        camera.position.z = lerp(camera.position.z, b[2], scalePercent(p[0], p[1]));
+  }
+});
+animationScripts.push({
+    start: p[1],
+    end: p[2],
+    func: function() {
+      camera.lookAt(0,0,0)
+      camera.position.set(b[0],b[1],b[2])
+    }
+  });
+// scalePercent(30, 40)
+animationScripts.push({
+  start: p[2],
+  end: p[3],
+  func: function() {
+    camera.lookAt(0,0,0)
+    camera.position.x = lerp(camera.position.x,c[0], scalePercent( p[2],  p[3]));
+    camera.position.y = lerp( camera.position.y,c[1], scalePercent(p[2],  p[3]));
+    camera.position.z = lerp(camera.position.z,c[2],scalePercent(p[2],  p[3]));
+  }
+});
+animationScripts.push({
+    start: p[3],
+    end: 100,
+    func: function() {
+      camera.lookAt(0,0,0)
+      camera.position.set(c[0],c[1],c[2])
+    }
+  });
+/* -------------------------------- scroll up ------------------------------- */
+const animationScriptsup = [];
+animationScriptsup.push({
+    start: 0,
+    end: 110,
+    func: function() {
+        camera.lookAt(0,0,0)
+        camera.position.x += ( mouseX/10000 - camera.position.x ) * .05;
+        camera.position.y += ( - mouseY/10000 - camera.position.y ) * .05;
+    }
+  });
+  animationScriptsup.push({
+  start: p[0],
+  end: p[1],
+  func: function() {
+    camera.lookAt(0,0,0)
+    camera.position.x = lerp(a[0],  camera.position.x, scalePercent(p[0], p[1]));
+        camera.position.y = lerp(a[1], camera.position.y, scalePercent(p[0], p[1]));
+        camera.position.z = lerp(a[2], camera.position.z, scalePercent(p[0], p[1]));
+  }
+});
+
+animationScriptsup.push({
+    start: 0,
+    end: 110,
+    func: function() {
+        camera.lookAt(0,0,0)
+        camera.position.x += ( mouseX/10000 - camera.position.x ) * .05;
+        camera.position.y += ( - mouseY/10000 - camera.position.y ) * .05;
+    }
+  });
+
+animationScriptsup.push({
+  start: p[2],
+  end: p[3],
+  func: function() {
+    camera.lookAt(0,0,0)
+    camera.position.x = lerp(b[0],camera.position.x, scalePercent(p[2], p[3]));
+    camera.position.y = lerp(b[1], camera.position.y, scalePercent(p[2], p[3]));
+    camera.position.z = lerp(b[2], camera.position.z,scalePercent(p[2], p[3]));
+  }
+});
+
+document.addEventListener( 'mousemove', onDocumentMouseMove );
+/* -------------------------------------------------------------------------- */
+/*                         tick and auxiliary functions                       */
+/* -------------------------------------------------------------------------- */
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime()
+
+    stats.update()
+    // console.log(elapsedTime)
+
+
+
+				camera.lookAt( 0,0,0 );
+
+       
+    isScrollingDown().then(function(isScrollingDown) {
+        if (isScrollingDown) {
+            ScrollDownAnimations()
+            console.log("down")
+        }  else if (isScrollingDown === false) {
+            console.log('Scrolling up by at least 50 pixels');
+            ScrollUpAnimations()
+            // ScrollDownAnimations()
+          } else {
+            console.log('Not scrolling');
+            // NoScrollAnimations()
+            ScrollDownAnimations()
+          }
+      });
+  
+    renderer.render(scene, camera)
+
+    cpx.innerHTML="x: " + camerapos.x.toString()
+    cpy.innerHTML="y: " + camerapos.y.toString()
+    cpz.innerHTML="z: " + camerapos.z.toString()
+
+    window.requestAnimationFrame(tick)
+}
+
 tick()
+
+
+function ScrollDownAnimations() {
+    animationScripts.forEach(function(a) { 
+        // time = timea
+      if (scrollPercent >= a.start && scrollPercent < a.end) {
+        a.func();
+      }
+    });
+  }
+
+  function ScrollUpAnimations() {
+    animationScriptsup.forEach(function(a) { 
+      if (scrollPercent >= a.start && scrollPercent < a.end) {
+        a.func();
+      }
+    });
+  }
+//   function ScrollUpAnimations() {
+//     animationScriptsup.forEach(function(a) { 
+//       if (scrollPercent >= a.start && scrollPercent < a.end) {
+//         a.func();
+//       }
+//     });
+//   }
+
+
+const gridHelper = new THREE.GridHelper( 10, 10 );
+// scene.add( gridHelper );
+const axesHelper = new THREE.AxesHelper( 5 );
+axesHelper.setColors (  0xff0000, 0xffff00, 0x0000ff)
+// scene.add( axesHelper );
 
 function lerp(x, y, a) {
     return (1 - a) * x + a * y
 }
-// Used to fit the lerps to start and end at specific scrolling percentages
+
 function scalePercent(start, end) {
-    return (scrollPercent - start) / (end - start)
+    let reTurn = (scrollPercent - start) / (end - start)
+    reTurn = clamp(reTurn,0,1)
+    return reTurn
+}
+function clamp(value, min, max) {
+    return Math.min(Math.max(value, min), max);
+  }
+
+function onDocumentMouseMove( event ) {
+    mouseX = ( event.clientX - windowHalfX ) * 4;
+    // mouseX = event.clientX
+    mouseY = ( event.clientY - windowHalfY ) * 4;
+    // mouseY = event.clientY
+}
+
+//   var prevScrollY = window.scrollY;
+//   var prevDirection = true;
+  
+  function isScrollingDown() {
+    // Get the current scroll position
+    var currentScrollY = window.scrollY;
+  
+    // Wait for a short period of time to get the next scroll position
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        // Get the next scroll position
+        var nextScrollY = window.scrollY;
+  
+        // Calculate the difference between the current and next scroll positions
+        var scrollDiff = nextScrollY - currentScrollY;
+  
+        // Determine if we're scrolling down by at least 50 pixels
+        if (scrollDiff > 0) {
+          // If the scroll difference is greater than 50, we're scrolling down
+          resolve(true);
+        } else if (scrollDiff < -0.08) {
+          // If the scroll difference is less than -50, we're scrolling up
+          resolve(false);
+        } else {
+          // If the scroll difference is within the threshold, we're not scrolling
+          resolve(null);
+        }
+      }, 100);
+    });
+  }
+
+
+ function getValue() {
+  let startTime = Date.now();  // get current time in milliseconds
+  let endTime = startTime + 1000;  // 1 second later
+  let a = 0;  // initial value of a
+  
+  // loop until 1 second has elapsed
+  while (Date.now() < endTime) {
+    let elapsedTime = Date.now() - startTime;  // time elapsed since start
+    a = elapsedTime / 1000;  // calculate new value of a
+  }
+  
+  // wait 10 milliseconds
+  setTimeout(function() {
+    return a;
+  }, 10);
 }
